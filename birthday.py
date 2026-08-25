60f396226d4b42a8087ea6e649f28dc4d3276bcc
theo-johansson-ntig <theo.johansson@elev.ga.ntig.se>
2026-08-25 13:35:01 +0200

    test

---
+++ birthday.py
@@ -0,0 +1,24 @@
+import random
+import datetime
+        
+start_date = datetime.date(2009, 1, 1)
+end_date = datetime.date(2009, 12, 31)
+list_dates = []
+chosen_dates = []
+delta = datetime.timedelta(days=1)
+
+while start_date <= end_date:
+    list_dates.append(start_date)
+    start_date += delta
+
+chosen_dates.append(random.choice(list_dates))
+
+while True:
+    random_day = random.choice(list_dates)
+    for i in chosen_dates:
+        if i == random_day:
+            chosen_dates.append(random_day)
+            print(f"\n{len(chosen_dates)}\n{random_day}")
+            exit()
+    else:
+        chosen_dates.append(random_day) 
