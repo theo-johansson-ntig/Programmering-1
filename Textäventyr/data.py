8f324eabdd2daa14c671a1ca3d9db9a4097f1239
theo-johansson-ntig <theo.johansson@elev.ga.ntig.se>
2026-09-25 12:02:41 +0200

    WIP textäventyr

--- Textäventyr/data.py
+++ Textäventyr/data.py
@@ -1,17 +1,27 @@
-player = {
-    "hp": 100,
-    "dmg": 10,
-    "weapon": None,
-    "armor": None
-}
+# Weapons
+sword = 1.25
 
-zombie = {
-    "hp": 25,
-    "dmg":7,
-    "weapon": None,
-    "armor": None
-}
+# Armor
+breastplate = 0.8
 
+# Entities
+class player:
+    hp = 100
+    dmg = 10
+    weapon = None
+    armor = None
 
-sword = 1.5
-breastplate = 0.01
+class zombie:
+    hp = 25
+    dmg = 7
+    weapon = None
+    armor = None
+
+class boss:
+    hp = 250
+    dmg = 25
+    weapon = None
+    armor = None
+
+# Actions
+action = ("Fight", "Random Weapon", "Random Armor", "Rest")
--- Textäventyr/textaventyr.py
+++ Textäventyr/textaventyr.py
@@ -1,7 +1,13 @@
 import data
 import random
 
-def damage(char, target):
-    char_weapon = char["weapon"]
-    target_armor = target["armor"]
-    return random.uniform(0.8, 1.25) * char["dmg"]
+def damage(attacker, target):
+    attacker_weapon = attacker.weapon or 1
+    target_armor = target.armor or 1
+    return round(attacker.dmg * attacker_weapon * target_armor * random.uniform(0.8, 1.25))
+
+while True:
+    player = data.player()
+
+    print(f"Current health: {player.hp}")
+    input(f"1) {data.action[0]} 2) {data.action[1]} 3) {data.action[2]} 4) {data.action[3]}\n")
